748a3a2e83fe187a0ff71f4a7564065c35871e4c
bnoguchi <brian.noguchi@gmail.com>
2010-08-03 18:57:41 -0700

    Added a package.json file for npm installs.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var assert = require("assert"),
-    bsearch = require("../build/default/binarySearch").binarySearch;
+    bsearch = require("binarySearch").binarySearch;
 arr = [11,12,13,14,15,16,17,18,19,20];
 var index = bsearch(arr, 16);
 assert.equal(arr[index], 16);
